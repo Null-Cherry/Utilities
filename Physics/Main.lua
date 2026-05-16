7b0f5f024145f821c84e5de99bfdaf9f081f7791
Null Cherry <sk4ndi1488@gmail.com>
2026-05-16 19:45:39 +0300

    Update Main.lua

--- Physics/Main.lua
+++ Physics/Main.lua
@@ -1,5 +1,8 @@
-if getgenv().PhyLib then
-	return getgenv().PhyLib
+local libN = "PhyLib"
+local g = (getfenv().getgenv or function() return _G end)()
+
+if g[libN] then
+	return g[libN]
 end
 
 local pack = table.pack
@@ -17,6 +20,7 @@ local rawset = rawset
 local find = table.find
 local wait = task.wait
 local unpack = table.unpack
+local random = math.random
 local v3 = vector.create
 local deg = math.deg
 local ncf = CFrame.new
@@ -24,8 +28,10 @@ local xyz = CFrame.fromEulerAnglesXYZ
 local rad = math.rad
 local look = CFrame.lookAt
 local workspace = workspace
+local clamp = math.clamp
 
 local zero = v3(0, 0, 0)
+local cfz = ncf()
 
 local ino = getfenv().isnetworkowner
 local ghp = getfenv().gethiddenproperty
@@ -96,47 +102,53 @@ local event = smt({
 
 local beforeSpoofing, afterSpoofing, afterSpoofCycle = event.new(), event.new(), event.new()
 
-local spoofQueue = { }
+local function fr()
+	return (random() - 0.5) * 2
+end
 
+local spoofQueue = { }
 local spooferBase = {
 	Set = function(self)
 		if not self.Enabled or not self.Object then return end
-		
+
 		local props = self.Properties
 		local object = self.Object
 		local originalValues = self.OriginalValues
-		
+
 		originalValues.CFrame = object.CFrame
 		originalValues.Velocity = object.AssemblyLinearVelocity
 		originalValues.RotVelocity = object.AssemblyAngularVelocity
-		
-		local originalPosition = props.Position or object.Position
-		local originalRotation = props.Rotation or object.Rotation
-		
-		local originalCFrame = ncf(originalPosition) * xyz(originalRotation.X, originalRotation.Y, originalRotation.Z)
 
-		local offsetPosition = props.OffsetPosition or zero
-		local offsetRotation = props.OffsetRotation or zero
-
-		local rot = originalRotation + v3(offsetRotation.Y, offsetRotation.X, offsetRotation.Z)
-		
-		object.CFrame = (ncf(originalPosition) * xyz(rad(rot.X), rad(rot.Y), rad(rot.Z))) + (originalCFrame.XVector * offsetPosition.X) + (originalCFrame.YVector * offsetPosition.Y) + (originalCFrame.ZVector * offsetPosition.Z) + (props.WorldOffsetPosition or zero)
-		object.AssemblyLinearVelocity = (props.AssemblyLinearVelocity or object.AssemblyLinearVelocity) + (props.OffsetVelocity or zero)
+		object.CFrame = (props.CFrame or object.CFrame) * (props.OffsetCFrame or cfz)
+		object.AssemblyLinearVelocity = (props.Velocity or object.AssemblyLinearVelocity) + (props.OffsetVelocity or zero) + v3(fr() / 2, fr() / 2, fr() / 2)
 		object.AssemblyAngularVelocity = (props.RotVelocity or object.AssemblyAngularVelocity) + (props.OffsetRotVelocity or zero)
+		
+		originalValues.NewCFrame = object.CFrame
 	end,
 	Restore = function(self)
 		if not self.Object then return end
-		
+
 		local object = self.Object
+		local props = self.Properties
 		local originalValues = self.OriginalValues
 		
-		for i, v in originalValues do
-			object[i == "RotVelocity" and "AssemblyAngularVelocity" or i] = v
-		end
+		local newCFrame = originalValues.NewCFrame
+		originalValues.NewCFrame = nil
 		
+		if newCFrame then
+			if (object.Position - newCFrame.Position).Magnitude >= 0.01 then -- teleported
+				originalValues.CFrame = nil
+				originalValues.Velocity = props.Velocity and props.Velocity + v3(fr() / 2, fr() / 2, fr() / 2) or originalValues.Velocity or zero
+			end
+		end
+
+		for i, v in originalValues do
+			object[i == "RotVelocity" and "AssemblyAngularVelocity" or i] = typeof(v) == "Vector3" and (v + v3(0, 0.01)) or v
+		end
+
 		clear(originalValues)
 	end,
-	
+
 	Unlink = function(self)
 		local found = find(spoofQueue, self)
 		if found then
@@ -152,30 +164,6 @@ spooferBase = {
 			rawset(self, index, not not value)
 		elseif index == "Object" then
 			rawset(self, index, typeof(value) == "Instance" and value or nil)
-		elseif index == "CFrame" then
-			if typeof(value) == "Vector3" then
-				self.Properties.Position = value
-				self.Properties.Rotation = nil
-			elseif typeof(value) == "CFrame" then
-				local x, y, z = value:ToEulerAnglesXYZ()
-				self.Properties.Position = value.Position
-				self.Properties.Rotation = v3(deg(x), deg(y), deg(z))
-			else
-				self.Properties.Position = nil
-				self.Properties.Rotation = nil
-			end
-		elseif index == "OffsetCFrame" then
-			if typeof(value) == "Vector3" then
-				self.Properties.OffsetPosition = value
-				self.Properties.OffsetRotation = nil
-			elseif typeof(value) == "CFrame" then
-				local x, y, z = value:ToEulerAnglesXYZ()
-				self.Properties.OffsetPosition = value.Position
-				self.Properties.OffsetRotation = v3(deg(x), deg(y), deg(z))
-			else
-				self.Properties.OffsetPosition = nil
-				self.Properties.OffsetRotation = nil
-			end
 		else
 			self.Properties[index] = value
 		end
@@ -185,7 +173,7 @@ spooferBase = {
 local newSpoofer = function(object)
 	local self = smt({ Properties = { }, OriginalValues = { }, Enabled = true, Object = object }, spooferBase)
 	insert(spoofQueue, self)
-	
+
 	return self
 end
 
@@ -194,7 +182,8 @@ local hb = rs.Heartbeat
 local rns = rs.RenderStepped
 local s = rs.Stepped
 
-local plr = game:GetService("Players").LocalPlayer
+local plrs = game:GetService("Players")
+local plr = plrs.LocalPlayer
 local playerSpoofer = newSpoofer(plr.Character and plr.Character:FindFirstChild("HumanoidRootPart") or nil)
 playerSpoofer.Enabled = false
 
@@ -209,7 +198,7 @@ end)
 task.spawn(function()
 	while hb:Wait() do
 		if not lib.Spoofer.Enabled then continue end
-		
+
 		beforeSpoofing:Fire()
 		for i, v in spoofQueue do
 			v:Set()
@@ -217,16 +206,30 @@ task.spawn(function()
 
 		afterSpoofing:Fire()
 		rns:Wait()
+
 		for i, v in spoofQueue do
 			v:Restore()
 		end
-
+		
 		s:Wait()
-		for i, v in spoofQueue do
-			v:Restore()
-		end
-
 		afterSpoofCycle:Fire()
+	end
+end)
+
+local prev, old
+beforeSpoofing:Connect(function()
+	if playerSpoofer.Object then
+		old = playerSpoofer.Object.CFrame.Position
+	end
+end)
+
+afterSpoofCycle:Connect(function()
+	if not lib.Spoofer.AdjustCamera or not lib.Spoofer.Enabled then return end
+
+	local cam = workspace.CurrentCamera
+	if playerSpoofer.Object and cam.CFrame ~= prev and old then
+		cam.CFrame += (playerSpoofer.Object.CFrame.Position - old)
+		prev = cam.CFrame
 	end
 end)
 
@@ -234,45 +237,19 @@ lib = freeze({
 	Spoofer = {
 		Spoof = function(self, object) return newSpoofer(object) end,
 		Enabled = true,
+		AdjustCamera = true,
 		Objects = spoofQueue,
 
 		SpoofPlayer = function(self)
 			playerSpoofer.Enabled = true
 			return playerSpoofer
 		end,
-		
+
 		BeforeSpoofing = beforeSpoofing,
 		AfterSpoofing = afterSpoofing,
 		AfterSpoofCycle = afterSpoofCycle
-	},
-
-	Predict = function(self, part)
-		if part:IsA("Model") then
-			part = part.PrimaryPart or part:FindFirstChild("HumanoidRootPart") or part:FindFirstChildWhichIsA("BasePart")
-		end
-
-		if not part or not part:IsA("BasePart") or not part:IsDescendantOf(workspace) then
-			return plr.Character and plr.Character:GetPivot() or ncf()
-		end
-
-		local pos = part.Position
-
-		if part.Anchored then
-			return part.CFrame
-		end
-
-		return part.CFrame * ncf(part.AssemblyLinearVelocity * (plr:GetNetworkPing() / 2))
-	end,
-
-	IsNetworkOwner = function(self, part)
-		if ino then
-			return ino(part)
-		end
-
-		local currentNetworkOwner = ghp and ghp(part, "NetworkOwnerV3")
-		return (typeof(currentNetworkOwner) == "number" and currentNetworkOwner > 2 or part.ReceiveAge == 0) and not part.Anchored
-	end
+	}
 })
 
-getgenv().PhyLib = lib
+g[libN] = lib
 return lib
